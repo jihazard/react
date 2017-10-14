2c95b99cde16ded3438252b15e1f3b2f5d053326
jihwan <yoonjh238@gmail.com>
2017-10-15 02:29:14 +0900

    state 업데이트

--- src/App.js
+++ src/App.js
@@ -3,23 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie.js';
 
-const movies =[
-  {
-  
-  title:"Matrix",
-  poster: "http://movie.phinf.naver.net/20170915_299/1505458505658vbKcN_JPEG/movie_image.jpg"
-  },
-  {
-  
-    title:"oldboy",
-    poster: "http://movie.phinf.naver.net/20131114_260/1384391087931T5Yx8_JPEG/movie_image.jpg"
-    }
-  ,{
-  
-    title:"starwars",
-    poster: "http://movie.phinf.naver.net/20170915_299/1505458505658vbKcN_JPEG/movie_image.jpg"
-    }
- ]   
+
 
 class App extends Component {
    //컴포넌트 실행 순서 : render : compoentwillmount() -> render() -> componentdidmount();
@@ -31,14 +15,60 @@ class App extends Component {
   }
 
   componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+         movies:[
+           ...this.state.movies,
+           {title: "trainspotting", poster:"http://movie.phinf.naver.net/20170915_299/1505458505658vbKcN_JPEG/movie_image.jpg"}
+         ]
+      })
+      console.log('hello setTimeout')
+    },3000)
+
+    setTimeout(() =>{
+     // 직접적으로 state 변경하면안도미 this.state.greeting= 'somethig'
+     //5초 후에 바뀌게 하기
+     /*
+     this.setState({
+         greeting: 'hello didmount'
+       })
+    }, 5000)
+    */
+    this.setState({
+      greeting: 'hello didmount'
+    })
+    })
     console.log("3.did mount")
   }
+
+  state={
+    greeting : 'hello',
+    movies : [
+      {
+      
+      title:"Matrix",
+      poster: "http://movie.phinf.naver.net/20170915_299/1505458505658vbKcN_JPEG/movie_image.jpg"
+      },
+      {
+      
+        title:"oldboy",
+        poster: "http://movie.phinf.naver.net/20131114_260/1384391087931T5Yx8_JPEG/movie_image.jpg"
+        }
+      ,{
+      
+        title:"starwars",
+        poster: "http://movie.phinf.naver.net/20170915_299/1505458505658vbKcN_JPEG/movie_image.jpg"
+        }
+     ]   
+  }
+
 
   render() {
     console.log("2.render`")
     return (  
       <div className="App">
-        {movies.map((movie , index)=>{
+        {this.state.greeting}
+        {this.state.movies.map((movie , index)=>{
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>
